daee7233e2c97600d614f1753cac795d5aa2d40b
VP-1718 <vikashpanda1718@gmail.com>
2025-04-06 11:40:52 +0000

    modifying the user schema

--- models/User.model.js
+++ models/User.model.js
@@ -1,32 +1,50 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+// const mongoose = require('mongoose');
+// const {Schema} = mongoose;
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    bloodGroup: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true
-    }
-})
+// const userSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     phone: {
+//         type: Number,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     bloodGroup: {
+//         type: String,
+//         required: true,
+//     },
+//     address: {
+//         type: String,
+//         required: true
+//     },
+//     gender: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+// module.exports = mongoose.model('User', userSchema);
+
+
+// new code
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ['donor', 'receiver'], default: 'donor' },
+  bloodGroup: String,
+  location: String,
+  contact: String,
+  lastDonationDate: Date,
+  isAvailable: { type: Boolean, default: true }
+});
 
 module.exports = mongoose.model('User', userSchema);
